23d2eafac15c0f154fbb952d14ab098df81d2a3d
deephx <deepaksingh@phx.co.in>
2026-01-31 17:04:52 +0530

    chore: remove unused admin editor components and UI helpers

--- components/admin/ui/TableLoader.jsx
+++
@@ -1,9 +0,0 @@
-export default function TableLoader() {
-  return (
-    <div>
-      <div className="spinner-border text-primary" role="status">
-        <span className="sr-only">Loading...</span>
-      </div>
-    </div>
-  );
-}
